2c80d569d9067457a0f9ab2ec99e761897af69c2
Ula Jeziorna <jeziornaula@wp.pl>
2021-12-06 11:57:40 +0100

    Stworzenie wstępnego layout'u z ostylowaniem css

--- src/components/Crypto.js
+++ src/components/Crypto.js
@@ -4,13 +4,8 @@ import './css/Crypto.css'
 // get our fontawesome imports
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-// import { bitcoin } from "@fortawesome/free-solid-svg-icons";
-
-
-// // Creating an Icon Library
-// import { library } from '@fortawesome/fontawesome-svg-core';
-
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faBitcoin } from "@fortawesome/free-brands-svg-icons";
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 
 
@@ -21,18 +16,18 @@ const Crypto = () => {
 
 
   return (
-    <>
-      <header>
-        {/* <FontAwesomeIcon icon={['fab', 'f379']} /> */}
-        {/* <faImage icon={['fab', 'f379']} /> */}
-        {/* <img src="bitcoin-brands.svg" alt="bitcoin" /> */}
-        {/* <img class="fab fa-bitcoin" /> */}
-        {/* <i class="fab fa-bitcoin"></i> */}
-        {/* <FontAwesomeIcon icon={'fabf379'} /> */}
-        <FontAwesomeIcon icon={faCoffee} />
-        <h1>Crypto Rate</h1>
+    <section class="container-crypto">
+      <header class="header-crypto">
+        <FontAwesomeIcon icon={faBitcoin} />
+        <h1 class="title-crypto">Crypto Rate</h1>
       </header>
-    </>
+      <div class="crypto-filter-container">
+      <input class="inp-crypto-filter" placeholder="Filter"></input>
+      </div>
+      <section>
+      
+      </section>
+    </section>
   )
 }
 
