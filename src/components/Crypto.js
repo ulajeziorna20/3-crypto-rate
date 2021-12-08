b666ad8d13a94521efacfe68efc1b7e22b47f9de
Ula Jeziorna <jeziornaula@wp.pl>
2021-12-08 12:20:08 +0100

    Proba stworzenia listy z obiektem cryptoRate -- nic nie dziala

--- src/components/Crypto.js
+++ src/components/Crypto.js
@@ -17,34 +17,83 @@ import axios from "axios";
 
 const Crypto = () => {
 
-  const [data, setDate] = useState()
+  const [cryptoRates, setCryptoRates] = useState({})
+  const [isOn, setIsOn] = useState(false)
 
 
 
   useEffect(() => {
-    const interval = setInterval(() => { getDate() }, 3000);
+    const interval = setInterval(() => { getCryptoRates() }, 3000);
+    // console.log(`on`);
 
+    // console.log(`off`);
     return () => clearInterval(interval);
+
   }, []);
 
 
+  // useEffect(() => {
+  //   console.log(`moj useEffect`);
+  // })
 
+  // useEffect(() => {
+  //   if (isOn) {
+  //     console.log(`IsOn!`);
+  //   }
+  // })
+
+
+  // useEffect(() => {
+  //   if (isOn === false) {
+  //     console.log(`IsOff!`);
+  //   }
+  // })
 
 
 
   // pobieranie danych o kursach walut
-  const getDate = () => {
+  const getCryptoRates = () => {
     axios.get('https://blockchain.info/ticker', {
-      mode: 'corse'
+      mode: 'corse',
     })
       .then(res => {
         // console.log(res.data);
 
-        const coursesObj = res.data;
-        // console.log(coursesObj);
+        const ratesObj = res.data;
+        console.log(ratesObj);
 
-        setDate(coursesObj)
-        console.log(data);
+
+
+        setCryptoRates(() => {
+          let newCryptoList = [];
+
+          for (const [tickerElem, cryptoRate] of Object.entries(ratesObj)) {
+            let lastCryptoList = cryptoRates.find((cryptoObj) => {
+              return (cryptoObj.currency === tickerElem);
+            });
+
+
+
+            let newObj = {
+              currency: tickerElem,
+              symbol: cryptoRate.symbol,
+              sell: cryptoRate.sell,
+              buy: cryptoRate.buy,
+              lastRatePrice: cryptoRate.last,
+
+            }
+
+            newCryptoList.push(newCryptoObj);
+
+          }
+
+        
+
+        })
+
+
+
+
       })
   }
 
@@ -55,6 +104,9 @@ const Crypto = () => {
   return (
     <section className="container-crypto">
       <header className="header-crypto">
+        <button onClick={() => {
+          setIsOn((isOn) => !isOn)
+        }}>{isOn ? `off` : `on`}</button>
         <FontAwesomeIcon icon={faBitcoin} />
         <h1 className="title-crypto">Crypto Rate</h1>
       </header>
@@ -62,9 +114,8 @@ const Crypto = () => {
         <input className="inp-crypto-filter" placeholder="Filter"></input>
       </div>
       <section>
-        <CryptoList />
+        <CryptoList cryptoRates={cryptoRates} />
       </section>
-      <CryptoList data={data} />
     </section>
   )
 }
--- src/components/CryptoList.js
+++ src/components/CryptoList.js
@@ -5,6 +5,13 @@ import './css/CryptoList.css';
 
 const CryptoList = (props) => {
 
+  // let cryptoCoursesList = props.data.map((ticker) => {
+
+  //   // return (
+  //   //   <li className="ticker-item">Last rate: {ticker.}</li>
+  //   // )
+  // })
+
   
   return (
     <>
